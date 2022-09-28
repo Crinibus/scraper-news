d693b0a8aa5b6932b84e8c14ae0a6e24fad41e68
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-09-28 22:58:15 +0200

    Add scrape_news_threads function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import threading
 from typing import List
 import logging.config
 from scraper_news import ScraperNews, request_url, argparse_setup, Filemanager
@@ -8,7 +9,10 @@ def main():
     args = argparse_setup()
 
     if args.scrape:
-        scrape_news(args.sources, args.breaking)
+        if args.threads:
+            scrape_news_threads(args.sources, args.breaking)
+        else:
+            scrape_news(args.sources, args.breaking)
 
 
 def scrape_news(sources: List[str], is_breaking: bool):
@@ -17,6 +21,25 @@ def scrape_news(sources: List[str], is_breaking: bool):
         s.get_news()
         s.print_news()
         s.save_news()
+
+
+def scrape_news_threads(sources: List[str], is_breaking: bool):
+    news_sources = [ScraperNews(source, is_breaking) for source in sources]
+
+    threads = [threading.Thread(target=news_source.get_news) for news_source in news_sources]
+
+    # Start scraping on all threads
+    for thread in threads:
+        thread.start()
+
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
+
+    # Print and save news (sequentially)
+    for news_source in news_sources:
+        news_source.print_news()
+        news_source.save_news()
 
 
 if __name__ == "__main__":
